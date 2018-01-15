52cbdca37887c014b4365014213854e1f886571a
Brian Hunley <brian@hunleyconsulting.com>
2018-01-15 14:16:08 -0500

    setup app for heroku

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -1,6 +1,8 @@
 const mongoose = require('mongoose');
 
 mongoose.Promise = global.Promise;
-mongoose.connect('mongodb://localhost:27017/TodoApp', { useMongoClient: true });
+
+const db = process.env.MONGODB_URI || 'mongodb://localhost:27017/TodoApp';
+mongoose.connect(db, { useMongoClient: true });
 
 module.exports = { mongoose };
