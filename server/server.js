c39da60485cd0df8ff306dcaac87a87996f2982d
Brian Hunley <brian@hunleyconsulting.com>
2018-01-15 07:05:46 -0500

    add GET /todos/:id

--- server/server.js
+++ server/server.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const { ObjectID } = require('mongodb');
 
 const { mongoose } = require('./db/mongoose');
 const { Todo } = require('./models/todo');
@@ -26,6 +27,31 @@ app.get('/todos', (req, res) => {
     res.send({todos});
   }, (err) => {
     res.status(400).send(err);
+  });
+});
+
+// GET /todos/123412341234
+app.get('/todos/:id', (req, res) => {
+  const id = req.params.id;
+
+  // check if todo has a validate id using isValid
+  if (!ObjectID.isValid(id)) {
+    // todo id is not valid, send back a 404 with an empty body
+    return res.status(404).send();
+  }
+
+  // findById
+  Todo.findById(id).then((todo) => {
+    // check if a todo was found
+    if (!todo) {
+      // if a todo was not found, send a 404 with an empty body
+      return res.status(404).send();
+    }
+
+    // a todo was found, so send a 200 with the todo in the body
+    res.status(200).send({todo});
+  }).catch((err) => {
+    res.status(400).send(); // error on findById function
   });
 });
 
