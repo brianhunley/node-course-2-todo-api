a0edf6d370a81cdd4b1930e43330af2378b3cc32
Brian Hunley <brian@hunleyconsulting.com>
2018-01-15 16:38:27 -0500

    add DELETE /todos/:id route

--- server/server.js
+++ server/server.js
@@ -57,6 +57,38 @@ app.get('/todos/:id', (req, res) => {
   });
 });
 
+// DELETE /todos/123123123
+  // remove todo by id
+  //   success
+  //     if no doc, send 404
+  //     if doc, send doc back with a 200
+  //   error - send 400 with an empty body
+app.delete('/todos/:id', (req, res) => {
+  // get the id
+  const id = req.params.id;
+
+  // validate the id -> not valid? return 404
+  if (!ObjectID.isValid(id)) {
+    // todo id is not valid, send back a 404 with an empty body
+    return res.status(404).send();
+  }
+
+  Todo.findByIdAndRemove(id).then((todo) => {
+    // check if no todo was return, showing it was removed
+    if (!todo) {
+      return res.status(404).send();
+    }
+
+    // a todo was found, so send a 200 with the todo in the body
+    res.status(200).send({todo});
+  }).catch((err) => {
+    res.status(400).send(); // error on findByIdAndRemove function
+  });
+});
+
+
+
+
 app.listen(port, () => {
   console.log(`Server started on port ${port}`);
 });
