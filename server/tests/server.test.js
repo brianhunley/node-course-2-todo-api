4dcf0ae4fc7aa77c9d924cd560af716d1a447f5e
Brian Hunley <brian@hunleyconsulting.com>
2018-01-16 06:06:54 -0500

    Test the DELETE /todos/:id route

--- server/tests/server.test.js
+++ server/tests/server.test.js
@@ -95,9 +95,50 @@ describe('GET /todos/:id', () => {
       .end(done);
   });
 
-  it('should return a 404 for non-object ids', (done) => {
+  it('should return a 404 if the object id is invalid', (done) => {
     request(app)
       .get('/todos/123abc')
+      .expect(404)
+      .end(done);
+  });
+});
+
+describe('DELETE /todos/:id', () => {
+  it('should remove a todo', (done) => {
+    const testId = todos[1]._id.toHexString();
+    
+    request(app)
+      .delete(`/todos/${testId}`)
+      .expect(200)
+      .expect((res) => {
+        expect(res.body.todo._id).toBe(testId);
+      })
+      .end((err, res) => {
+        if (err) {
+          return done(err);
+        }
+
+        // query database using findById - is should fail, since it was deleted
+        // use the toNotExist assurtion
+        Todo.findById(testId).then((todo) => {
+          expect(todo).toBeNull();
+          done();
+        }).catch((err) => done(err));
+      });
+  });
+
+  it('should return a 404 if todo not found', (done) => {
+    const newId = new ObjectID().toHexString();
+
+    request(app)
+      .delete(`/todos/${newId}`)
+      .expect(404)
+      .end(done);
+  });
+
+  it('should return a 404 if the object id is invalid', (done) => {
+    request(app)
+      .delete('/todos/123abc')
       .expect(404)
       .end(done);
   });
