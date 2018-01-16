52d7b600306ece5aea007fc335b8b4c3bec2e247
Brian Hunley <brian@hunleyconsulting.com>
2018-01-16 06:39:48 -0500

    add PATCH /todos/:id route

--- server/server.js
+++ server/server.js
@@ -1,3 +1,4 @@
+const _ = require('lodash');
 const express = require('express');
 const bodyParser = require('body-parser');
 const { ObjectID } = require('mongodb');
@@ -86,8 +87,37 @@ app.delete('/todos/:id', (req, res) => {
   });
 });
 
+// PATCH HTTP method to do document updates
+app.patch('/todos/:id', (req, res) => {
+  const id = req.params.id;
+  const body = _.pick(req.body, ['text', 'completed']);
 
+  // validate the id -> not valid? return 404
+  if (!ObjectID.isValid(id)) {
+    // todo id is not valid, send back a 404 with an empty body
+    return res.status(404).send();
+  }
 
+  if (_.isBoolean(body.completed) && body.completed) {
+    body.completedAt = new Date().getTime();
+  } else {
+    body.completed = false;
+    body.completedAt = null;
+  } 
+
+  Todo.findByIdAndUpdate(id, { $set: body }, { new: true }
+  ).then((todo) => {
+    // check if no todo was return, showing it was removed
+    if (!todo) {
+      return res.status(404).send();
+    }
+
+    // a todo was found, so send a 200 with the todo in the body
+    res.status(200).send({todo});    
+  }).catch((err) => {
+    res.status(400).send();
+  });  
+});
 
 app.listen(port, () => {
   console.log(`Server started on port ${port}`);
