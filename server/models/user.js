15cc21100c030a85af5f5277d92beabc13ba38d8
Brian Hunley <brian@hunleyconsulting.com>
2018-01-18 22:44:38 -0500

    add GET /users/me and authentication middleware

--- server/models/user.js
+++ server/models/user.js
@@ -45,6 +45,7 @@ UserSchema.methods.toJSON = function() {
   return _.pick(userObject, ['_id', 'email']);
 };
 
+// instance function - methods
 UserSchema.methods.generateAuthToken = function() {
   const user = this;
   const access = 'auth';
@@ -56,6 +57,24 @@ UserSchema.methods.generateAuthToken = function() {
     return token;
   });
 };
+
+// model function - statics
+UserSchema.statics.findByToken = function(token) {
+  const User = this;
+  let decoded;
+
+  try {
+    decoded = jwt.verify(token, 'abc123');
+  } catch (e) {
+    return Promise.reject();
+  }
+
+  return User.findOne({
+    '_id': decoded._id,
+    'tokens.token': token,
+    'tokens.access': 'auth'
+  })
+}
 
 const User = mongoose.model('User', UserSchema);
 
--- server/server.js
+++ server/server.js
@@ -8,6 +8,7 @@ const { ObjectID } = require('mongodb');
 const { mongoose } = require('./db/mongoose');
 const { Todo } = require('./models/todo');
 const { User } = require('./models/user');
+const { authenticate } = require('./middleware/authenticate');
 
 var app = express();
 
@@ -144,6 +145,10 @@ app.post('/users', (req, res) => {
     res.status(400).send(err);
   }); 
 });
+
+app.get('/users/me', authenticate, (req, res) => {
+  res.send(req.user);
+})
 
 // start web server listening on specified port
 app.listen(port, () => {
